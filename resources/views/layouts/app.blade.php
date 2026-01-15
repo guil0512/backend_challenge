ab85ac5dc0217ef60af9140ead7b56ef8319c8ec
Guilherme Olivo <guiolivo27004@gmail.com>
2026-01-15 10:09:16 -0300

    Início do projeto

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,6 @@
     @vite(['resources/sass/app.scss','resources/js/app.js'])
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
     <title>{{ config('app.name', 'Laravel') }}</title>
